3633ddaa1d642b8552008c6ee01e2acf5077c160
khaled elsaeed abdelfattah <khaledelsaied62@gmail.com>
2023-05-20 02:03:47 +0300

    buid pagination for table of products

--- admin_pages/Books.php
+++ admin_pages/Books.php
@@ -83,12 +83,14 @@ if(isset($_GET['timeout']) || !isset($admin_id)){
     <?php include('admin_sidenav.php'); ?>
 
     <main class="main" style="padding:0px">
-        <div class="buttons">
+        <div class="button">
             <a href="add_book.php" class="btn">Add New Book</a>
         </div>
         <div class="main_item all_books show">
-            <div class="table">
+            <div class="table_head">
                 <h3> Display All Books</h3>
+            </div>
+            <div class="table">
                 <table>
                     <thead>
                         <tr>
@@ -105,40 +107,78 @@ if(isset($_GET['timeout']) || !isset($admin_id)){
                     </thead>
                     <tbody>
                         <?php
-                        $query_2_products=mysqli_query($conn,"SELECT p.* ,c.category_name FROM products p INNER JOIN category c ON p.category_id=c.category_id");
+                        // Records per page is set to 5
+                        $pagesize = 10;      
+                        $start_rec =0;
+                        // calculate number of pages
+                        $books_query=mysqli_query($conn,"SELECT COUNT(*) as books_count FROM products");
+                        while ($count_books=mysqli_fetch_assoc($books_query)){
+                            echo "<script> console.log({$count_books['books_count']});</script>";
+                            $all_books=$count_books['books_count'];
+                        }
+
+                        $num_pages = ceil($all_books/$pagesize);
+                        echo "<script> console.log({$num_pages});</script>";
+
+                        // if the page is greater than the number of pages, display the last page
+                        if(isset($_REQUEST['page'])){
+                            if($_REQUEST['page']>$num_pages){
+                                $curr_page=1;
+                            }elseif($_REQUEST['page']<1){
+                                $curr_page=$num_pages;
+                            } else{
+                                $curr_page = $_REQUEST['page'];
+                            }
+                        }
+                        else{
+                            $curr_page = 1;
+                        }
+                        $privious_page=$curr_page-1;
+                        $next_page=$curr_page+1;
+
+
+                        // Calculating the starting record number
+                        $start_rec = (($curr_page-1) * $pagesize) ;
+
+
+                            
+                        // Content of the table
+                        $slno = $start_rec + 1;       
+                        $query_2_products=mysqli_query($conn,"SELECT p.* ,c.category_name FROM products p INNER JOIN category c ON p.category_id=c.category_id LIMIT $start_rec, $pagesize");
                         if(mysqli_num_rows($query_2_products)>0){
                             
+                            
                             while($row_2_products=mysqli_fetch_assoc($query_2_products)){
-                                $i=1;
+                               $prod_id=$row_2_products['prod_id'];
                                 echo 
-                            "<tr>
-                                <td>$i</td>
-                                <td>{$row_2_products['prod_name']}</td>
-                                <td>{$row_2_products['category_name']}</td>
-                                <td>{$row_2_products['prod_quant']}</td>
-                                <td>{$row_2_products['price']}</td>
-                                <td>{$row_2_products['status']}</td>
-                                <td>{$row_2_products['prod_imag_url']}</td>
-                                <td>update</td>
-                                <td>delete</td>
-                            </tr>";
-                            $i++;
+                                "<tr id='row-$prod_id'>
+                                    <td>$slno</td>
+                                    <td>{$row_2_products['prod_name']}</td>
+                                    <td>{$row_2_products['category_name']}</td>
+                                    <td>{$row_2_products['prod_quant']}</td>
+                                    <td>{$row_2_products['price']}</td>
+                                    <td>{$row_2_products['status']}</td>
+                                    <td><img src='{$row_2_products['prod_imag_url']}' alt=''></td>
+                                    <td><button type='button' id='update_btn' name='update_product' class='update_btn'>update</button></td>
+                                    <td><button type='button' id='delete_btn' name='delete_product' class='delete_btn'>delete</button></td>
+                                </tr>";
+                                $slno++;
                             }
+
                         }
                         ?>
                     </tbody>
                 </table>
             </div>
             <div class="table_pagnetion">
-                <a href="#">&lt;privious</a>
-                <a href="#" class="active">1</a>
-                <a href="#">2</a>
-                <a href="#">3</a>
-                <a href="#">4</a>
-                <a href="#">5</a>
-                <a href="#">6</a>
-                <a href="#">7</a>
-                <a href="#">next&gt;</a>
+                <?php
+                
+                echo "<a href='?page=$privious_page & per-pages=$num_pages'>&lt;privious</a>";
+                for($i=1;$i<=$num_pages;$i++){
+                    echo "<a href='?page=$i& per-pages=$num_pages'>$i</a>";
+                }
+                echo "<a href='?page=$next_page & per-pages=$num_pages'>next&gt;</a>";
+                ?>
             </div>
         </div>
         
